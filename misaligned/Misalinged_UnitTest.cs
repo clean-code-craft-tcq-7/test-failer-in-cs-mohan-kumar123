13516a5a710cc7b61fa4c6a093a3715af23596c0
QOH2KOR <QOH2KOR@BMHE1115826>
2022-12-08 19:32:05 +0530

    Misalinged fixes

--- misaligned/Misalinged_UnitTest.cs
+++ misaligned/Misalinged_UnitTest.cs
@@ -10,10 +10,11 @@ namespace MisalignedSpace
             //Arrange & Act & Assert
             int result = Misaligned.printColorMap();
             Debug.Assert(result == 25);
-            Debug.Assert(Misaligned.formatColorPair(1, 1) == "7  | Red   | Orange");
-            Debug.Assert(Misaligned.formatColorPair(0, 0) == "1  | White | Blue");
-            Debug.Assert(Misaligned.formatColorPair(0, 4) == "5  | White | Slate");
-            Debug.Assert(Misaligned.formatColorPair(2, 3) == "14 | Black | Brown");
+            Debug.Assert(Misaligned.formatColorPair(1, 1) == "7  | Red    | Orange");
+            Debug.Assert(Misaligned.formatColorPair(0, 0) == "1  | White  | Blue");
+            Debug.Assert(Misaligned.formatColorPair(0, 4) == "5  | White  | Slate");
+            Debug.Assert(Misaligned.formatColorPair(2, 3) == "14 | Black  | Brown");
+            Debug.Assert(Misaligned.formatColorPair(5, 6) == "Color which your looking for not available please try other color.");
             Console.WriteLine("All is well (maybe!)");
         }
     }
--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace MisalignedSpace {
     public partial class Misaligned {
-        public static string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
+         public static string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
         public static string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
 
         public static int printColorMap()
@@ -20,17 +21,52 @@ namespace MisalignedSpace {
         }
         public static string formatColorPair(int i, int j)
         {
-            string colorpair;
-            if (i * 5 + j + 1 < 10)
+            string colorpair = "";
+            int baseIpt = i * 5 + j + 1;
+            try
             {
-                colorpair = String.Format("{0}  | {1} | {2}", i * 5 + j + 1, Misaligned.majorColors[i], Misaligned.minorColors[j]);
+                colorpair = Misaligned.patternGenerationLogic(baseIpt, Misaligned.majorColors[i], Misaligned.minorColors[j]);
             }
-            else
+            catch (Exception e)
             {
-                colorpair = String.Format("{0} | {1} | {2}", i * 5 + j + 1, Misaligned.majorColors[i], Misaligned.minorColors[j]);
+
+                colorpair = "Color which your looking for not available please try other color.";
             }
             return colorpair;
         }
+
+        private static string patternGenerationLogic(int baseInput, string majorColor, string minorColor)
+        {
+            string resultValue = "Colour Not Availibile";
+            string mjrLgth = majorColor.ToString().Length.ToString();
+            string constructvalue = baseInput < 10 ? "1"+ mjrLgth : "2"+ mjrLgth;
+
+            IDictionary<string, string> myDict = Misaligned.SetDictionaryValues(baseInput, majorColor, minorColor);
+
+            if (myDict.ContainsKey(constructvalue))
+            {
+                resultValue = myDict[constructvalue];
+            }
+            
+            return resultValue;
+        }
+
+        private static IDictionary<string, string> SetDictionaryValues(int baseInput, string majorColor, string minorColor)
+        {
+            // 5 white  2  = 7
+            // 3 red    4  = 7
+            // 5 black  2  = 7
+            // 6 yellow 1  = 7
+            IDictionary<string, string> myDict = new Dictionary<string, string>();
+            myDict.Add("15", String.Format("{0}  | {1}  | {2}", baseInput, majorColor, minorColor));
+            myDict.Add("13", String.Format("{0}  | {1}    | {2}", baseInput, majorColor, minorColor));
+            myDict.Add("25", String.Format("{0} | {1}  | {2}", baseInput, majorColor, minorColor));
+            myDict.Add("23", String.Format("{0} | {1}    | {2}", baseInput, majorColor, minorColor));
+            myDict.Add("26", String.Format("{0} | {1} | {2}", baseInput, majorColor, minorColor));
+
+            return myDict;
+        }
+
         static void Main(string[] args)
         {
            Misalinged_UnitTest.Test_printColorMap_SuccessCase();
