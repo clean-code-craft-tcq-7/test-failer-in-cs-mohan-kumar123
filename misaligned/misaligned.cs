ffcb0372d4108bdc58a7f4239f620951985992a4
QOH2KOR <QOH2KOR@BMHE1115826>
2022-12-08 11:36:19 +0530

    Reverted the actual code

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -3,37 +3,21 @@ using System.Diagnostics;
 
 namespace MisalignedSpace {
     class Misaligned {
-        string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
-        string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
-
-       static int printColorMap()
-        {
+        static int printColorMap() {
+            string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
+            string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
             int i = 0, j = 0;
-            for (i = 0; i < 5; i++)
-            {
-                for (j = 0; j < 5; j++)
-                {
-                    Console.WriteLine(formatColorPair(i, j));
+            for(i = 0; i < 5; i++) {
+                for(j = 0; j < 5; j++) {
+                    Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
                 }
             }
             return i * j;
         }
-        public static string formatColorPair(int i, int j)
-        {
-            string colorpair;
-            if (i * 5 + j + 1 < 10)
-            {
-                colorpair = String.Format("{0}  | {1} | {2}", i * 5 + j + 1, majorColors[i], minorColors[j]);
-            }
-            else
-            {
-                colorpair = String.Format("{0} | {1} | {2}", i * 5 + j + 1, majorColors[i], minorColors[j]);
-            }
-            return colorpair;
-        }
-        static void Main(string[] args)
-        {
-            test_ColorPair();
+        static void Main(string[] args) {
+            int result = printColorMap();
+            Debug.Assert(result == 27);
+            Console.WriteLine("All is well (maybe!)");
         }
     }
 }
--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 namespace TshirtSpace {
     class Tshirt {
         static string Size(int cms) {
-            if(cms <= 38) {
+            if(cms < 38) {
                 return "S";
             } else if(cms > 38 && cms < 42) {
                 return "M";
@@ -13,9 +13,9 @@ namespace TshirtSpace {
             }
         }
         static void Main(string[] args) {
-            Debug.Assert(Size(37) == "S");
-            Debug.Assert(Size(40) == "M");
-            Debug.Assert(Size(43) == "L");
+            Debug.Assert(Size(38) == "S");
+            Debug.Assert(Size(42) == "M");
+            Debug.Assert(Size(41) == "L");
             Console.WriteLine( "All is well (maybe!)");
         }
     }
