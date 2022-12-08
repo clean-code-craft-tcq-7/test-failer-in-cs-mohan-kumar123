7e8ad6844469cd5bceeafc63c4078ddfec27dd82
QOH2KOR <QOH2KOR@BMHE1115826>
2022-12-08 11:50:40 +0530

    test folder and file

---
+++ UnitTest/Tshirt_Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using TshirtSpace.TshirtSpace;
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Tshirt_Test
+{
+	
+		public int Test_TshirtSize_SuccessCase()
+        {
+		//Arrange & Act & Assert
+		Debug.Assert(Tshirt.Size(37) == "S");
+		Debug.Assert(Tshirt.Size(40) == "M");
+		Debug.Assert(Tshirt.Size(43) == "L");
+		Debug.Assert(Tshirt.Size(38) == "S");
+		Debug.Assert(Tshirt.Size(42) == "L");
+		Console.WriteLine("All is well (maybe!)");
+
+		}
+		
+}
