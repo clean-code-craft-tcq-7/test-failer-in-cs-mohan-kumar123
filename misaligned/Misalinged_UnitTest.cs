850a96ca21c041315543a6841019ce84761326ea
QOH2KOR <QOH2KOR@BMHE1115826>
2022-12-08 16:23:30 +0530

    output alingment

--- misaligned/Misalinged_UnitTest.cs
+++ misaligned/Misalinged_UnitTest.cs
@@ -10,7 +10,7 @@ namespace MisalignedSpace
             //Arrange & Act & Assert
             int result = Misaligned.printColorMap();
             Debug.Assert(result == 25);
-            Debug.Assert(Misaligned.formatColorPair(1, 1) == "7  | Red | Orange");
+            Debug.Assert(Misaligned.formatColorPair(1, 1) == "7  | Red   | Orange");
             Debug.Assert(Misaligned.formatColorPair(0, 0) == "1  | White | Blue");
             Debug.Assert(Misaligned.formatColorPair(0, 4) == "5  | White | Slate");
             Debug.Assert(Misaligned.formatColorPair(2, 3) == "14 | Black | Brown");
