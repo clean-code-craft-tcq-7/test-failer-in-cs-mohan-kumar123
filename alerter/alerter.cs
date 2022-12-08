8f02239b9827725ff6f828e21f580291fd83acf1
QOH2KOR <QOH2KOR@BMHE1115826>
2022-12-08 13:32:02 +0530

    err fix

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 
 namespace AlerterSpace {
     public partial class Alerter {
-        static int alertFailureCount = 0;
+       public static int alertFailureCount = 0;
 
         public virtual int networkAlertStub(float celcius)
         {
@@ -17,14 +17,15 @@ namespace AlerterSpace {
 
        public static void alertInCelcius(float farenheit) 
         {
+            Alerter alerter =  new Alerter();
             float celcius = (farenheit - 32) * 5 / 9;
-            int returnCode = networkAlertStub(celcius);
+            int returnCode = alerter.networkAlertStub(celcius);
             if (returnCode != 200) {
                 // non-ok response is not an error! Issues happen in life!
                 // let us keep a count of failures to report
                 // However, this code doesn't count failures!
                 // Add a test below to catch this bug. Alter the stub above, if needed.
-                alertFailureCount += 0;
+                Alerter.alertFailureCount += 0;
             }
         }
         static void Main(string[] args) {
