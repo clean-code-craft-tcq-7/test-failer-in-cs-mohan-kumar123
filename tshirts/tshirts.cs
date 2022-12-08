b54da4f4f0f6ebf34a69255cbc8bd32f0abb908e
QOH2KOR <QOH2KOR@BMHE1115826>
2022-12-08 12:01:01 +0530

    Organized the test class

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -14,12 +14,26 @@ namespace TshirtSpace {
             }
         }
         static void Main(string[] args) {
-            Tshirt_Test Tshirt_Test = new Tshirt_Test();
-            Tshirt_Test.Test_TshirtSize_SuccessCase();
-            Debug.Assert(Size(38) == "S");
-            Debug.Assert(Size(42) == "M");
-            Debug.Assert(Size(41) == "L");
-            Console.WriteLine( "All is well (maybe!)");
+            Tshirt_Test tshirt_Test = new Tshirt_Test();
+            tshirt_Test.Test_TshirtSize_SuccessCase();
+           // Debug.Assert(Size(38) == "S");
+            //Debug.Assert(Size(42) == "M");
+            //Debug.Assert(Size(41) == "L");
+            //Console.WriteLine( "All is well (maybe!)");
         }
     }
+
+    public class Tshirt_Test
+	{
+		public int Test_TshirtSize_SuccessCase()
+		{
+			//Arrange & Act & Assert
+			Debug.Assert(Tshirt.Size(37) == "S");
+			Debug.Assert(Tshirt.Size(40) == "M");
+			Debug.Assert(Tshirt.Size(43) == "L");
+			Debug.Assert(Tshirt.Size(38) == "S");
+			Debug.Assert(Tshirt.Size(42) == "L");
+			Console.WriteLine("All is well (maybe!)");
+		}
+	}
 }
